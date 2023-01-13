ea08ced269823f43eb757f87e4dde83f0edfe211
Karthikey22 <mail2karthikeyan22@gmail.com>
2023-01-13 06:24:22 +0000

    [ci skip] Pushing deployment files for namespace, service accounts and clusterrolebinding
    Signed-off-by: Karthikey22 <mail2karthikeyan22@gmail.com>

--- run.sh
+++ run.sh
@@ -15,8 +15,8 @@ export PATH=/root/bin:$PATH
 #Path to k8s config file
 KUBECONFIG=/home/bevel/build/config
 
-echo "Validatin network yaml"
-ajv validate -s /home/bevel/platforms/network-schema.json -d /home/bevel/build/network.yaml 
+# echo "Validatin network yaml"
+# ajv validate -s /home/bevel/platforms/network-schema.json -d /home/bevel/build/network.yaml 
 
 echo "Running the playbook..."
 exec ansible-playbook -vv /home/bevel/platforms/shared/configuration/site.yaml --inventory-file=/home/bevel/platforms/shared/inventory/ -e "@/home/bevel/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
